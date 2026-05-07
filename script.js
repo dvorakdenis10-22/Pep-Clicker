57a6f0630ab699f77fef35f57a8bab485add2c4e
dvorakdenis10-22 <dvorakdenis10@gmail.com>
2026-05-07 21:16:08 +0200

    script.js

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
 let coins = 0;
 let perSec = 0;
-let userKey = "";
 
 // UI
+
 const coinsText = document.getElementById("coins");
-const perSecText = document.getElementById("perSec");
 const horseBtn = document.getElementById("horseBtn");
 
-// LOGIN
-function login() {
-const username = document.getElementById("username").value;
-const password = document.getElementById("password").value;
+// CLICK
 
-if (!username || !password) return;
-
-userKey = username + "_" + password;
-
-document.getElementById("loginBox").style.display = "none";
-document.getElementById("game").style.display = "block";
-
-loadGame();
-update();
-}
-
-// klik
 horseBtn.addEventListener("click", () => {
 coins++;
+
 update();
 saveGame();
 });
 
-// upgrady
+// UPGRADES
+
 function buyHorse() {
 if (coins >= 50) {
 coins -= 50;
 perSec += 1;
+
 update();
 saveGame();
 }
@@ -44,6 +31,7 @@ function buyStable() {
 if (coins >= 200) {
 coins -= 200;
 perSec += 5;
+
 update();
 saveGame();
 }
@@ -53,37 +41,50 @@ function buyFarm() {
 if (coins >= 1000) {
 coins -= 1000;
 perSec += 20;
+
 update();
 saveGame();
 }
 }
 
-// auto income
+// AUTO INCOME
+
 setInterval(() => {
 coins += perSec;
+
 update();
 saveGame();
 }, 1000);
 
-// update UI
+// UPDATE
+
 function update() {
-coinsText.innerText = Math.floor(coins) + " Pep Coins";
-perSecText.innerText = perSec + " / sec";
+coinsText.innerText = Math.floor(coins);
 }
 
 // SAVE
-function saveGame() {
-if (!userKey) return;
 
-localStorage.setItem(userKey + "_coins", coins);
-localStorage.setItem(userKey + "_perSec", perSec);
+function saveGame() {
+localStorage.setItem("pepCoins", coins);
+localStorage.setItem("pepPerSec", perSec);
 }
 
 // LOAD
-function loadGame() {
-const savedCoins = localStorage.getItem(userKey + "_coins");
-const savedPerSec = localStorage.getItem(userKey + "_perSec");
 
-if (savedCoins !== null) coins = Number(savedCoins);
-if (savedPerSec !== null) perSec = Number(savedPerSec);
+function loadGame() {
+const savedCoins = localStorage.getItem("pepCoins");
+const savedPerSec = localStorage.getItem("pepPerSec");
+
+if (savedCoins !== null) {
+coins = Number(savedCoins);
 }
+
+if (savedPerSec !== null) {
+perSec = Number(savedPerSec);
+}
+}
+
+// START
+
+loadGame();
+update();
